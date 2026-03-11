94a6bc6eaa6204e1e264615e4da95f44c30470f2
Stephan Merker <stephan.merker@sap.com>
2026-03-11 15:00:54 +0100

    Extra AWS ESC test case to control configuration
    - AWS ESC doesn't work w/o specifying region

---
+++ s3/integration/aws_esc_test.go
@@ -0,0 +1,128 @@
+package integration_test
+
+import (
+	"os"
+
+	"github.com/cloudfoundry/storage-cli/s3/config"
+	"github.com/cloudfoundry/storage-cli/s3/integration"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Testing for AWS European Sovereign Cloud region", func() {
+	Context("with AWS ESC (static creds) configurations", func() {
+		accessKeyID := os.Getenv("ACCESS_KEY_ID")
+		secretAccessKey := os.Getenv("SECRET_ACCESS_KEY")
+		bucketName := os.Getenv("BUCKET_NAME")
+		region := os.Getenv("REGION")
+		s3Host := os.Getenv("S3_HOST")
+
+		BeforeEach(func() {
+			Expect(accessKeyID).ToNot(BeEmpty(), "ACCESS_KEY_ID must be set")
+			Expect(secretAccessKey).ToNot(BeEmpty(), "SECRET_ACCESS_KEY must be set")
+			Expect(bucketName).ToNot(BeEmpty(), "BUCKET_NAME must be set")
+			Expect(region).ToNot(BeEmpty(), "REGION must be set")
+			Expect(s3Host).ToNot(BeEmpty(), "S3_HOST must be set")
+		})
+
+		configurations := []TableEntry{
+			Entry("with region and without host", &config.S3Cli{
+				AccessKeyID:     accessKeyID,
+				SecretAccessKey: secretAccessKey,
+				BucketName:      bucketName,
+				Region:          region,
+			}),
+			Entry("with folder", &config.S3Cli{
+				AccessKeyID:     accessKeyID,
+				SecretAccessKey: secretAccessKey,
+				BucketName:      bucketName,
+				FolderName:      "test-folder/a-folder",
+				Region:          region,
+			}),
+			Entry("with host style enabled", &config.S3Cli{
+				AccessKeyID:     accessKeyID,
+				SecretAccessKey: secretAccessKey,
+				BucketName:      bucketName,
+				Region:          region,
+				HostStyle:       true,
+			}),
+		}
+		DescribeTable("Blobstore lifecycle works",
+			func(cfg *config.S3Cli) { integration.AssertLifecycleWorks(s3CLIPath, cfg) },
+			configurations,
+		)
+
+		DescribeTable("Invoking `ensure-storage-exists` works",
+			func(cfg *config.S3Cli) { integration.AssertOnStorageExists(s3CLIPath, cfg) },
+			configurations,
+		)
+		DescribeTable("Blobstore bulk operations work",
+			func(cfg *config.S3Cli) { integration.AssertOnBulkOperations(s3CLIPath, cfg) },
+			configurations,
+		)
+		DescribeTable("Invoking `s3cli get` on a non-existent-key fails",
+			func(cfg *config.S3Cli) { integration.AssertGetNonexistentFails(s3CLIPath, cfg) },
+			configurations,
+		)
+		DescribeTable("Invoking `s3cli delete` on a non-existent-key does not fail",
+			func(cfg *config.S3Cli) { integration.AssertDeleteNonexistentWorks(s3CLIPath, cfg) },
+			configurations,
+		)
+		DescribeTable("Invoking `s3cli sign` returns a signed URL",
+			func(cfg *config.S3Cli) { integration.AssertOnSignedURLs(s3CLIPath, cfg) },
+			configurations,
+		)
+		DescribeTable("Multipart copy works with low threshold",
+			func(cfg *config.S3Cli) { integration.AssertMultipartCopyWorks(s3CLIPath, cfg) },
+			configurations,
+		)
+
+		configurations = []TableEntry{
+			Entry("with encryption", &config.S3Cli{
+				AccessKeyID:          accessKeyID,
+				SecretAccessKey:      secretAccessKey,
+				BucketName:           bucketName,
+				Region:               region,
+				ServerSideEncryption: "AES256",
+			}),
+			Entry("without encryption", &config.S3Cli{
+				AccessKeyID:     accessKeyID,
+				SecretAccessKey: secretAccessKey,
+				BucketName:      bucketName,
+				Region:          region,
+			}),
+		}
+		DescribeTable("Invoking `s3cli put` uploads with options",
+			func(cfg *config.S3Cli) { integration.AssertPutOptionsApplied(s3CLIPath, cfg) },
+			configurations,
+		)
+
+		Describe("Invoking `s3cli put` with arbitrary upload failures", func() {
+			It("returns the appropriate error message", func() {
+				cfg := &config.S3Cli{
+					AccessKeyID:     accessKeyID,
+					SecretAccessKey: secretAccessKey,
+					BucketName:      bucketName,
+					Host:            "http://localhost",
+				}
+				msg := "upload failure"
+				integration.AssertOnPutFailures(cfg, largeContent, msg)
+			})
+		})
+
+		Describe("Invoking `s3cli put` with multipart upload failures", func() {
+			It("returns the appropriate error message", func() {
+				cfg := &config.S3Cli{
+					AccessKeyID:     accessKeyID,
+					SecretAccessKey: secretAccessKey,
+					BucketName:      bucketName,
+					Region:          region,
+					MultipartUpload: true,
+				}
+				msg := "upload retry limit exceeded"
+				integration.AssertOnPutFailures(cfg, largeContent, msg)
+			})
+		})
+	})
+})
